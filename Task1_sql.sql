6a967b04b02eb2156983d927d5639e0abe4136c4
Nikita97-hub <nikitabhujade844@gmail.com>
2024-08-21 18:34:56 +0530

    Add files via upload
    TASK 1 completed

---
+++ Task1_sql.sql
@@ -0,0 +1,88 @@
+Create table Nikita_data (
+	id serial primary key,
+	name varchar(150),
+	age int,
+	location varchar,
+	Qualification varchar,
+    passing_year int,
+	status varchar,
+	percentage decimal
+
+)
+
+select * from Nikita_data
+
+INSERT INTO Nikita_data (id, name, age, location, Qualification, passing_year, status, percentage)
+VALUES (1, 'Nikita', 27, 'Nagpur','BE', 2019, 'married', 60);
+
+Insert INTO Nikita_data (id, name, age, location, Qualification, passing_year, status, percentage)
+VALUES (2, 'Nikhil',23,'sihore','MBA',2022,'unmarried',60);
+
+INSERT INTO Nikita_data (id, name, age, location, Qualification, passing_year, status, percentage)
+VALUES (3,'Rahul', 34,  'nagpur', 'MBBS', 2010, 'married', 60);
+
+INSERT INTO Nikita_data (id, name, age, location, Qualification, passing_year, status, percentage)
+VALUES (4,'pankaj', 36,  'nagpur', 'BE', 2018, 'married', 50);
+
+INSERT INTO Nikita_data (id, name, age, location, Qualification, passing_year, status, percentage)
+VALUES (4,'priyanka', 32,  'nagpur', 'BSE', 2012, 'married', 40);
+
+
+INSERT INTO Nikita_data values (5,'payal', 30,  'nagpur', 'BE', 2015, 'married', 65);
+
+INSERT INTO Nikita_data values (6,'bhagyrashree', 22,  'nagpur', 'BE', 2022, 'unmarried', 75);
+
+INSERT INTO Nikita_data values (7,'harsh', 21,  'nagpur', 'BCOM', 2023, 'unmarried', 45);
+
+INSERT INTO Nikita_data values (8,'Tushar', 22,  'Amravati', 'BCOM', 2012, 'married', 90);
+
+INSERT INTO Nikita_data values (9,'Akshay', 31,  'Amravati', 'BCOM', 2014, 'married', 80);
+
+INSERT INTO Nikita_data values (10,'Shailash', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+
+Select * from Nikita_data
+
+
+INSERT INTO Nikita_data values (11,'S1', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (12,'S2', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (13,'S3', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Nikita_data values (14,'S4', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (15,'S5', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (16,'S6', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (17,'S7', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (18,'S8', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Nikita_data values (19,'S9', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+
+INSERT INTO Nikita_data values (20,'S20', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (21,'S21', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (22,'S23', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Nikita_data values (23,'S24', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (24,'S25', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (25,'S26', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (26,'S27', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (27,'S28', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Nikita_data values (28,'S29', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (29,'S31', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (30,'S32', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (31,'S33', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Nikita_data values (32,'S34', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (33,'S35', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (34,'S36', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (35,'S37', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (36,'S38', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Nikita_data values (37,'S39', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (38,'S31', 23,  'Hydrabade', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (39,'S42', 20,  'Mumbai', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (40,'S43', 32,  'delhi', 'schooling', 2018, 'unmarried',45);
+INSERT INTO Nikita_data values (41,'S44', 26,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (42,'S45', 27,  'Hydrabad', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (43,'S46', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (44,'S47', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (45,'S48', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Nikita_data values (46,'S49', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (47,'S46', 28,  'pune', 'schooling', 2017, 'married',35);
+INSERT INTO Nikita_data values (48,'S47', 29,  'nagpur', 'BE', 2020, 'unmarried',35);
+INSERT INTO Nikita_data values (49,'S48', 16,  'nagpur', 'BCOM', 2011, 'unmarried',90);
+INSERT INTO Nikita_data values (50,'S49', 22,  'nagpur', 'BSE', 2020, 'unmarried',35);
+
+Select * from Nikita_data
